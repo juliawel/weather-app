94ac3fd9c885b17eac4b81c6537ba062a344e2db
juliawel <julia20.wel@hotmail.com>
2024-10-21 08:30:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ async function fetchWeatherData(city) {
     }
 }
 
+
 const cityElement = document.querySelector(".city");
 const temperature = document.querySelector(".temp");
 const windSpeed = document.querySelector(".wind-speed");
